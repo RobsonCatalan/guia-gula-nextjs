7028eed90907bbfb94354a335ab81485a29a2747
Robson Catalan <robson.catalan@gmail.com>
2025-04-27 13:07:31 -0300

    DetailPage: remove bottom padding from hero section

--- src/app/restaurante/[cidade]/restaurante/[nome-restaurante]/client-component.tsx
+++ src/app/restaurante/[cidade]/restaurante/[nome-restaurante]/client-component.tsx
@@ -305,7 +305,7 @@ export default function RestaurantDetailClient() {
           </ol>
         </nav>
       )}
-      <section className="text-white py-16 bg-gradient-to-b from-[#FF7A68] to-[#FFF8F0]">
+      <section className="text-white pt-16 bg-gradient-to-b from-[#FF7A68] to-[#FFF8F0]">
         <div className="max-w-7xl mx-auto text-center px-6">
           <h1 className="text-4xl md:text-5xl font-bold mb-4 font-['Roboto']">Restaurante {restaurant.name}</h1>
           {restaurant.mainPhoto && (

ba8806af59dde49126fe5af2ab50a0f4562da137
Robson Catalan <robson.catalan@gmail.com>
2025-04-27 12:57:27 -0300

    DetailPage: add spacing above tags

--- src/app/restaurante/[cidade]/restaurante/[nome-restaurante]/client-component.tsx
+++ src/app/restaurante/[cidade]/restaurante/[nome-restaurante]/client-component.tsx
@@ -320,7 +320,7 @@ export default function RestaurantDetailClient() {
           )}
           {/* Culinária section moved below photo */}
           {restaurant.categories && restaurant.categories.length > 0 && (
-            <div className="mb-4">
+            <div className="mt-4 mb-4">
               <div className="flex flex-wrap gap-2">
                 {restaurant.categories.map(code => (
                   <h2 key={code} className="inline-block m-0 !text-xs !font-normal !leading-none bg-[#F4A261] text-white px-2 py-1 rounded">

25202df42744b001eab221217eceadad7ea91bd0
Robson Catalan <robson.catalan@gmail.com>
2025-04-27 19:25:07 -0300

    fix: remove generateMetadata from restaurant detail page

--- src/app/restaurante/[cidade]/restaurante/[nome-restaurante]/page.tsx
+++ src/app/restaurante/[cidade]/restaurante/[nome-restaurante]/page.tsx
@@ -1,14 +1,4 @@
 import RestaurantDetailClient from './client-component';
-import type { Metadata } from 'next';
-
-export async function generateMetadata({ params }: { params: { 'nome-restaurante': string } }): Promise<Metadata> {
-  const slug = params['nome-restaurante'];
-  const name = slug
-    .split('-')
-    .map(w => w.charAt(0).toUpperCase() + w.slice(1))
-    .join(' ');
-  return { title: `Restaurante ${name} | Gula.menu` };
-}
 
 export default function Page() {
   return <RestaurantDetailClient />;

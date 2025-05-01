1eda6b99ef6d5af603fae5a89d7634dc19a9414a
Robson Catalan <robson.catalan@gmail.com>
2025-05-01 11:33:18 -0300

    chore: improve functions predeploy script

--- functions/predeploy.js
+++ functions/predeploy.js
@@ -1,5 +1,17 @@
 const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
-console.log('Running Next.js build for SSR function...');
-// Run build at project root (one level up)
-execSync('npm --prefix .. run build', { stdio: 'inherit' });
+const functionsDir = __dirname;
+const projectDir = path.resolve(functionsDir, '..');
+
+console.log('Cleaning functions .next and public directories...');
+fs.rmSync(path.join(functionsDir, '.next'), { recursive: true, force: true });
+fs.rmSync(path.join(functionsDir, 'public'), { recursive: true, force: true });
+
+console.log('Building Next.js project...');
+execSync('npm run build', { cwd: projectDir, stdio: 'inherit', shell: true });
+
+console.log('Copying artifacts to functions...');
+fs.cpSync(path.join(projectDir, '.next'), path.join(functionsDir, '.next'), { recursive: true });
+fs.cpSync(path.join(projectDir, 'public'), path.join(functionsDir, 'public'), { recursive: true });

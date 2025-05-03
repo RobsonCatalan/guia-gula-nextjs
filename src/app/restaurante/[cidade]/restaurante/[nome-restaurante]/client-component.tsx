f2d0816713e9c33df593ca789b3ead246fe379d7
Robson Catalan <robson.catalan@gmail.com>
2025-05-02 21:07:47 -0300

    Use standard red for category tags and stars

--- src/app/restaurante/[cidade]/restaurante/[nome-restaurante]/client-component.tsx
+++ src/app/restaurante/[cidade]/restaurante/[nome-restaurante]/client-component.tsx
@@ -179,7 +179,7 @@ export default function RestaurantDetailClient() {
           </svg>
           {/* Colored overlay clipped by width */}
           <span className="absolute top-0 left-0 h-full overflow-hidden" style={{ width: `${fill * 100}%` }}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="w-full h-full text-[#F4A261]" fill="currentColor" viewBox="0 0 20 20">
+            <svg xmlns="http://www.w3.org/2000/svg" className="w-full h-full text-[#FF5842]" fill="currentColor" viewBox="0 0 20 20">
               <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.165 3.584a1 1 0 00.95.69h3.768c.969 0 1.371 1.24.588 1.81l-3.047 2.213a1 1 0 00-.364 1.118l1.165 3.584c.3.921-.755 1.688-1.538 1.118l-3.047-2.213a1 1 0 00-1.176 0l-3.047 2.213c-.783.57-1.838-.197-1.538-1.118l1.165-3.584a1 1 0 00-.364-1.118L2.575 9.011c-.783-.57-.38-1.81.588-1.81h3.768a1 1 0 00.95-.69l1.165-3.584z" />
             </svg>
           </span>
@@ -237,7 +237,7 @@ export default function RestaurantDetailClient() {
             <div className="mt-4">
               <div className="flex flex-wrap gap-2">
                 {restaurant.categories.map(code => (
-                  <h2 key={code} className="inline-block m-0 !text-xs !font-normal !leading-none bg-[#F4A261] text-white px-2 py-1 rounded">
+                  <h2 key={code} className="inline-block m-0 !text-xs !font-normal !leading-none bg-[#FF5842] text-white px-2 py-1 rounded">
                     {categoryMap[code] || code}
                   </h2>
                 ))}

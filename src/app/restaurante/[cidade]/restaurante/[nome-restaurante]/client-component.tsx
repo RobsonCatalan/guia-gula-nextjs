c489f39e782a531302d3dca5446536c7fa35a4fe
Robson Catalan <robson.catalan@gmail.com>
2025-05-02 21:05:55 -0300

    Set restaurant detail hero h1 color to gray

--- src/app/restaurante/[cidade]/restaurante/[nome-restaurante]/client-component.tsx
+++ src/app/restaurante/[cidade]/restaurante/[nome-restaurante]/client-component.tsx
@@ -221,7 +221,7 @@ export default function RestaurantDetailClient() {
       )}
       <section className="py-16 bg-[#FFF8F0]">
         <div className="max-w-7xl mx-auto text-center px-6">
-          <h1 className="text-4xl md:text-5xl font-bold mb-4 font-['Roboto']">Restaurante {restaurant.name}</h1>
+          <h1 className="text-4xl md:text-5xl font-bold mb-4 font-['Roboto'] text-[#4A4A4A]">Restaurante {restaurant.name}</h1>
           {restaurant.mainPhoto && (
             <Image
               src={restaurant.mainPhoto}

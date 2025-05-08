702d9afb3f1b3e4d2d8ccc4b35e14dea5e46f980
Robson Catalan <robson.catalan@gmail.com>
2025-05-08 08:29:20 -0300

    Update gallery dots color and remove '-feira' from weekday names

--- src/app/restaurante/[cidade]/restaurante/[nome-restaurante]/client-component.tsx
+++ src/app/restaurante/[cidade]/restaurante/[nome-restaurante]/client-component.tsx
@@ -17,13 +17,13 @@ const formatSlug = (slug: string): string =>
     .join(' ')
     .replace(/\bSao\b/g, 'São');
 
-// Mapeamento de weekday para nome em PT
+// Mapeamento de weekday para nome em PT (sem sufixo '-feira')
 const weekdayNames: Record<number, string> = {
-  1: 'Segunda-feira',
-  2: 'Terça-feira',
-  3: 'Quarta-feira',
-  4: 'Quinta-feira',
-  5: 'Sexta-feira',
+  1: 'Segunda',
+  2: 'Terça',
+  3: 'Quarta',
+  4: 'Quinta',
+  5: 'Sexta',
   6: 'Sábado',
   7: 'Domingo',
 };
